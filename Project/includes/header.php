96abb2b74d51db2b9793cb13faf4bc865c7f0eb9
Nikolai312-lab <nikint478@gmail.com>
2021-10-11 10:24:34 +0300

    My first folder

---
+++ Project/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <ul>
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="contact.php">Contact</a>
+      </li>
+    </ul>
---
+++ Project/index.php
@@ -0,0 +1,7 @@
+<?php
+  include('includes/header.php')
+ ?>
+<h1>Home page!</h1>
+<?php
+  include('includes/footer.php')
+ ?>
